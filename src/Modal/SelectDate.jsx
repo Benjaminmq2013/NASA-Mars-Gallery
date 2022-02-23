0ccfc51d01024b6e56e71743eda3e2589c2b03cd
Adriel Benjamín Martínez <61093891+Benjaminmq2013@users.noreply.github.com>
2022-02-23 03:07:12 +0100

    Date control has been added

--- src/Modal/SelectDate.jsx
+++ src/Modal/SelectDate.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
+import M from "materialize-css"
+import SelectDateInput from './SelectDateInput';
+import menu_icon from "../assets/icons/menu.svg"
 
 const SelectDate = () => {
+
+    document.addEventListener("DOMContentLoaded", function () {
+        var elems = document.querySelectorAll(".modal");
+        var instances = M.Modal.init(elems);
+      });
+
   return (
     <>
-      <a className="waves-effect waves-light btn modal-trigger" href="#modal1">
-        Modal
+      <a className="waves-effect waves-light btn-floating btn-large modal-trigger" href="#modal-select-date" id="modalDate-btn">
+        <img src={ menu_icon } alt="" />
       </a>
 
       <div id="modal-select-date" className="modal bottom-sheet">
-        <div className="modal-content">
-          <h4>Modal Header</h4>
-          <p>A bunch of text</p>
+        <div className="modal-content blue lighten-1">
+          <h4 className="white-text">Select a date to get more images</h4>
+          <SelectDateInput />
         </div>
-        <div className="modal-footer">
-          <a href="#!" className="modal-close waves-effect waves-green btn-flat">
+        <div className="modal-footer blue lighten-1 ">
+          <a href="#!" className="modal-close waves-effect waves-green btn-flat white-text">
             Agree
           </a>
         </div>
---
+++ src/Modal/SelectDateInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SelectDateInput = () => {
+  return (
+    <input type="date" className="white-text" id="date-input"/>
+  )
+}
+
+export default SelectDateInput
